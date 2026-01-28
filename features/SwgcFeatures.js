0d14a9e23a236563233079b373c6c76bdbda13b1
fahmyzzx <54723004+FAHMYZAR@users.noreply.github.com>
2026-01-29 02:56:16 +0700

    Update SwgcFeatures.js

--- features/SwgcFeatures.js
+++ features/SwgcFeatures.js
@@ -8,7 +8,7 @@ const crypto = require('crypto');
 class GroupStatusFeature {
     constructor() {
         this.name = 'swgc';
-        this.description = '_Update Status Grup (simple & stabil)_';
+        this.description = '_Update SwGc_';
         this.ownerOnly = false;
     }
 
